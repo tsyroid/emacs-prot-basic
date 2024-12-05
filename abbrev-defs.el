1b51329d25741ce45034ec0859d6abfe1bc7ebdd
Taz <tsyroid@gmail.com>
2024-12-05 08:39:26 -0800

    Update histories (FIX THIS)

---
+++ abbrev-defs.el
@@ -0,0 +1,15 @@
+;;-*-coding: utf-8;-*-
+(define-abbrev-table 'global-abbrev-table
+  '(
+    ("medyn" "" my-current-time :count 2)
+    ("megit" "https://github.com/tsyroid/" nil :count 0)
+    ("metest" "a global abbrev for demo purposes" nil :count 8)
+    ("tzfull" "Taz Syroid (taz@syroidmanor.com)" nil :count 1)
+    ("tzname" "Taz Syroid (TZ)" nil :count 0)
+   ))
+
+(define-abbrev-table 'text-mode-abbrev-table
+  '(
+    ("me2test" "a text-mode-only abbrev for demo purposes" nil :count 0)
+   ))
+
